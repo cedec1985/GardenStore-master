f1a5a6118e138cef5be7c89bfc695a98afb628a0
Cédric De Craim <cedricdecraim@msn.com>
2024-09-18 14:12:23 +0200

    bon

--- gardenstore/src/app/app.module.ts
+++ gardenstore/src/app/app.module.ts
@@ -40,8 +40,7 @@ import { HttpClient } from '@angular/common/http';
 @NgModule({
   declarations: [],
   
-  imports: [
-    CommonModule,AppComponent,NavbarComponent, HomeComponent,ProfiluserModule, FormsModule, ReactiveFormsModule, HttpClient, RouterModule,
+  imports: [CommonModule,AppComponent,NavbarComponent, HomeComponent,ProfiluserModule, FormsModule, ReactiveFormsModule, HttpClient, RouterModule,
     RegisterComponent,ConnexionComponent,CartComponent,AdminComponent,OutillageComponent,MobilierComponent,PlantesComponent,AccessoiresComponent,
     AchattermineComponent,ConfirmationComponent,ProduitresultComponent,CategoryComponent, ProfilUserComponent,BrowserAnimationsModule,
     ProfiluserRoutingModule,ProfiluserModule,ProductDetailsComponent,OutillagedetailsComponent,MobilierdetailsComponent,PlantesdetailsComponent,AccessoiresdetailsComponent,StockEpuiseDirective,ContactComponent,ProfilUserDisplayComponent],
